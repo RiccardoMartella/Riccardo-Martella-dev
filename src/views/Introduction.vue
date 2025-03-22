76945b19ce1f600da5cf8d0e7e19cf4037b43592
Riccardo Martella <riccardomartella77@gmail.com>
2025-03-22 17:34:20 +0100

    add: introduction video

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -44,8 +44,16 @@
             <div class="featured-visual position-relative overflow-visible">
               <div class="video-container">
                 <div class="video-overlay"></div>
-                <video class="featured-video" src="/public/videos/introduction2.mp4" autoplay muted loop></video>
-
+                <div class="responsive-video-wrapper">
+                  <iframe 
+                    src="https://www.youtube.com/embed/juwy6bU34io?autoplay=1&mute=1&controls=0&disablekb=1&loop=1&modestbranding=1&playsinline=1&rel=0&showinfo=0&playlist=juwy6bU34io&iv_load_policy=3&fs=0&vq=hd1080&hd=1" 
+                    title="Assembly Station" 
+                    frameborder="0" 
+                    allow="autoplay;" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen>
+                  </iframe>
+                </div>
 
                 <div class="interactive-elements">
                   <div class="ui-element ui-top-right">
@@ -553,6 +561,25 @@ export default {
   box-shadow: 0 20px 40px rgba(0, 0, 0, 0.2);
   z-index: 1;
   margin: 0 auto;
+  background-color: transparent;
+}
+
+.responsive-video-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.responsive-video-wrapper iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border: none;
+  pointer-events: none;
 }
 
 .featured-video {
