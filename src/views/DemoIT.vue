937051648adbfaf8d9f7c630c2e3a139ffde3175
Riccardo Martella <riccardomartella77@gmail.com>
2025-04-13 17:09:11 +0200

    fix:demo

--- src/views/DemoIT.vue
+++ src/views/DemoIT.vue
@@ -156,7 +156,7 @@ export default {
     return {
       demoLaunched: false,
       isLoading: false,
-        demoUrl: 'https://assembly-statio.com/Build-Assembly/index.html'
+        demoUrl: '/Build-Assembly/index.html',
     };
   },
   methods: {
